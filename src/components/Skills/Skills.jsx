d2ce34ee1e7bc0f3dd05600298882a23151e10a3
WebDeverDan <danielbvideo@gmail.com>
2023-06-25 20:04:13 -0400

    updated path for images in skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,22 +13,22 @@ const Skills = () => {
             <p>Ruby on Rails | Javascrip | JQuery | Node | React | HTML | CSS | Bootstrap | PostgreSQL | MySQL | Github | Bitbucket | Heroku | Digital Ocean</p>
       </Slide>
         <div className="toolbox">
-            <img src="/images/icons8-ruby-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-ruby-on-rails-64.png" class='codeIcon'></img>
-            <img src="/images/icons8-javascript-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-jquery-50.png" class='codeIcon'></img>
-            <img src="/images/icons8-node-js-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-react-native-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-html-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-css-100.png" class='codeIcon'></img>
-            <img src="/images/icons8-bootstrap-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-mongodb-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-graphql-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-postgresql-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-mysql-50 (1).png" class='codeIcon'></img>
-            <img src="/images/icons8-github-50.png" class='codeIcon'></img>
-            <img src="/images/icons8-bitbucket-48.png" class='codeIcon'></img>
-            <img src="/images/icons8-heroku-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-ruby-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-ruby-on-rails-64.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-javascript-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-jquery-50.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-node-js-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-react-native-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-html-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-css-100.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-bootstrap-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-mongodb-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-graphql-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-postgresql-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-mysql-50 (1).png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-github-50.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-bitbucket-48.png" class='codeIcon'></img>
+            <img src="/react-portfolio/images/icons8-heroku-48.png" class='codeIcon'></img>
         </div>
         <Slide direction="right" id="tools">
             <p>For additional tools and skills, please request my resume via the email below</p>
